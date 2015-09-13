ad00d660e4ff24e3f7206dd5d76d0c9fdf4f85c0
Ivan Portilla <jicuastumalp@unal.edu.co>
2015-09-13 16:58:43 -0500

    Pusheando mi rama

--- testApp/db/schema.rb
+++ testApp/db/schema.rb
@@ -11,13 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150913211500) do
-
-  create_table "usuarios", force: true do |t|
-    t.string   "nombre"
-    t.string   "username"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
