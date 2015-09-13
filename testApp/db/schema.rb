617a8a9ade7f72388eb5d96c33777154acf667fd
Ivan Portilla <jicuastumalp@unal.edu.co>
2015-09-13 17:23:24 -0500

    Agregado password para usuario

--- testApp/db/schema.rb
+++ testApp/db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150913220918) do
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "username"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
+  end
 
 end
